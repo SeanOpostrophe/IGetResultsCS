e4c917ae66b7b6205cbc4a4fd7fcfff8b39d8590
Sean Opostrophe <77934702+SeanOpostrophe@users.noreply.github.com>
2021-03-16 16:27:19 -0400

    Update Main.go

--- Main.go
+++ Main.go
@@ -8,24 +8,35 @@ import (
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	var stat1 int = 1
-	var stat2 int = 1
-	var chk1 int = rand.Intn(stat1) + 1
-	var chk2 int = rand.Intn(stat2) + 1
-	var chkt int = chk1 + chk2
+	var die1 int = 1
+	var die2 int = 1
+	var result1 int = rand.Intn(die1) + 1
+	var result2 int = rand.Intn(die2) + 1
+	var resultT int = result1 + result2
 	var col int = 0
-	if chk1 == 1 {
+	if result1 == 1 {
 		col = col + 1
 	}
-	if chk1 == stat1 {
+	if result1 == die1 {
 		col = col + 1
 	}
-	if chk2 == 1 {
+	if result2 == 1 {
 		col = col + 1
 	}
-	if chk2 == stat2 {
+	if result2 == die2 {
 		col = col + 1
 	}
-	fmt.Println("You rolled a", chk1, "and a", chk2, "for a total of", chkt, "and caused", col, "collateral")
-
+	fmt.Println("You rolled a", resultT)
+	var heatlvl int = 0
+	heat := [8]int{0, 10, 12, 10, 8, 6, 4, 1}
+	for h := 0; h <= heatlvl; h++ {
+		var heatT int = rand.Intn(heat)
+		if heatT == heat[h] {
+			col = col + 1
+		}
+		if heatT == 1 {
+			col = col + 1
+			fmt.PrintLn("and the heat generated", heatT, "for a total of", (resultT + heatT), col, "collateral was generated")
+		}
+	}
 }
