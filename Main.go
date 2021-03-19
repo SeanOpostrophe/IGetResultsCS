bab9d4cde27d195ed494c07ddac2f16150370238
jarodonnell <jarodonnell@gmail.com>
2021-03-19 16:50:19 -0400

    Update Main.go

--- Main.go
+++ Main.go
@@ -3,43 +3,48 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"os"
+	"strconv"
 	"time"
 )
 
+//  a pool of two dice comprises a stat
+//  collateral increases when minimum or maximum rolled
 func main() {
+	argLength := len(os.Args[1:])
+	fmt.Printf("Arg length is %d\n", argLength)
+
+	for i, a := range os.Args[1:] {
+		fmt.Printf("Arg %d is %s\n", i+1, a)
+	}
+	programName := os.Args[0]
+	dieOne := os.Args[1]
+	dieTwo := os.Args[1:][1]
+	fmt.Println("You entered a", dieOne, "and a", dieTwo, "for ", programName)
+	die1, _ := strconv.Atoi(dieOne)
+	die2, _ := strconv.Atoi(dieTwo)
+
 	rand.Seed(time.Now().Unix())
-	var die1 int = 6
-	var die2 int = 12
-	var heatlvl int = 1
-	var result1 int = rand.Intn(die1) + 1
-	var result2 int = rand.Intn(die2) + 1
-	var resultT int = result1 + result2
+
+	var dieOneSides int = die1
+	var dieTwoSides int = die2
+	var roll1 int = rand.Intn(dieOneSides) + 1
+	var roll2 int = rand.Intn(dieTwoSides) + 1
+	var rollt int = roll1 + roll2
 	var col int = 0
-	if result1 == 1 {
+	if roll1 == 1 {
 		col = col + 1
-	}
-	if result1 == die1 {
-		col = col + 1
-	}
-	if result2 == 1 {
-		col = col + 1
-	}
-	if result2 == die2 {
-		col = col + 1
-	}
 
-	var heatT int = 0
-	heat := 8 //[8]int{0, 10, 12, 10, 8, 6, 4, 1}
-	for h := 0; h <= heatlvl; h++ {
-		var heatGen int = rand.Intn(heat)
-		heatT = heatT + heatGen
-		if heatGen == heat /*[h]*/ {
-			col = col + 1
-		}
-		if heatGen == 1 {
-			col = col + 1
-
-		}
 	}
-	fmt.Println("You rolled a", resultT, "and the heat generated", heatT, "for a total of", (resultT + heatT), "and", col, "collateral was caused")
+	if roll1 == dieOneSides {
+		col = col + 1
+	}
+	if roll2 == 1 {
+		col = col + 1
+	}
+	if roll2 == dieTwoSides {
+		col = col + 1
+	}
+	fmt.Println("You rolled a", roll1, "and a", roll2, "for a total of", rollt, "and caused", col, "collateral")
+
 }
