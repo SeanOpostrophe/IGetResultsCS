46f27b6b62611cc952915a0850f7676aa6530e44
Sean Opostrophe <77934702+SeanOpostrophe@users.noreply.github.com>
2021-03-24 13:23:32 -0400

    Update Main.go
    separated heat and colateral into different functions gave a +1 to heat die var so it would give correct range

--- Main.go
+++ Main.go
@@ -33,6 +33,12 @@ func main() {
 	var roll1 int = rand.Intn(dieOneSides) + 1
 	var roll2 int = rand.Intn(dieTwoSides) + 1
 	var rollt int = roll1 + roll2
+	var col int = collateral(roll1, roll2, dieOneSides, dieTwoSides)
+	var col2, heatTotal int = calculateHeat(heatLvl)
+
+	fmt.Println("You rolled a", roll1, "and a", roll2, "and the heat generated", heatTotal, "for a total of", (rollt + heatTotal), "and caused", col+col2, "collateral")
+}
+func collateral(roll1 int, roll2 int, dieOneSides int, dieTwoSides int) int {
 	var col int = 0
 	if roll1 == 1 {
 		col = col + 1
@@ -47,19 +53,24 @@ func main() {
 	if roll2 == dieTwoSides {
 		col = col + 1
 	}
-	var heatT int = 0
-	heatLvlDie := 8 //[8]int{0, 10, 12, 10, 8, 6, 4, 1}
-
+	return col
+}
+func calculateHeat(heatLvl int) (col2 int, heatTotal int) {
+	heatTotal = 0
+	col2 = 0
+	heatLvlDie := [8]int{10, 12, 10, 8, 6, 4, 1}
 	for h := 0; h <= heatLvl; h++ {
-		var heatDie int = heatLvlDie //[h]
-		var heatGen int = rand.Intn(heatDie)
-		heatT = heatT + heatGen
+
+		var heatDie int = heatLvlDie[h]
+		var heatGen int = rand.Intn(heatDie) + 1
+		heatTotal = heatTotal + heatGen
+		fmt.Println("heat gen is", heatGen, "out of", heatDie)
 		if heatGen == heatDie {
-			col = col + 1
+			col2 = col2 + 1
 		}
 		if heatGen == 1 {
-			col = col + 1
+			col2 = col2 + 1
 		}
 	}
-	fmt.Println("You rolled a", roll1, "and a", roll2, "and the heat generated", heatT, "for a total of", (rollt + heatT), "and caused", col, "collateral")
+	return col2, heatTotal
 }
